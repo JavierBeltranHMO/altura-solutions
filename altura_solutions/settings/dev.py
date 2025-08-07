6ee4cc31b6d498e8554f8c63b47dcae9b6d44944
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-06 19:05:36 -0700

    basic project setup

---
+++ altura_solutions/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-%idw95=pcxh!$0+h0rvt!z6*@_+%v07l^(dw@^=e-i=)dxc_ys"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
