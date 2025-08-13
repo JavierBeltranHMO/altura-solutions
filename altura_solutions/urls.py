763431ccf3272826bc7ca600e215a699decd957e
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-13 16:29:45 -0700

    fixed typo

--- altura_solutions/urls.py
+++ altura_solutions/urls.py
@@ -25,13 +25,16 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns +=[
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     path("", include(wagtail_urls)),
-    path('__debug__/', include(debug_toolbar.urls)),
+    
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
