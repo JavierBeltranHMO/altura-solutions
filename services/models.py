78effa9d31a0f7c898ec7c014ca6cdfb53fc1ea4
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-12 17:41:20 -0700

    changed get-context and added for loop for services

--- services/models.py
+++ services/models.py
@@ -14,6 +14,11 @@ class ServiceListingPage(Page):
         FieldPanel("subtitle"),
     ]
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context["services"] = ServicePage.objects.live().public()
+        return context
+
 
 class ServicePage(Page):
     description = models.TextField(
