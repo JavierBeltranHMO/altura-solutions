e73f472aa73f3e3ec13e12429c9e663fbbd3733e
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-15 16:26:14 -0700

    custom table options

--- home/models.py
+++ home/models.py
@@ -7,6 +7,31 @@ from wagtail.snippets.blocks import SnippetChooserBlock
 
 import streams.blocks as blocks
 
+new_table_options={
+      'minSpareRows': 0,
+    'startRows': 3,
+    'startCols': 3,
+    'colHeaders': False,
+    'rowHeaders': True,
+    'contextMenu': [
+        'row_above',
+        'row_below',
+        '---------',
+        'col_left',
+        'col_right',
+        '---------',
+        'remove_row',
+        'remove_col',
+        '---------',
+        'undo',
+        'redo'
+    ],
+    'editor': 'text',
+    'stretchH': 'all',
+    'height': 108,
+    'renderer': 'text',
+    'autoColumnSize': False,
+}
 
 class HomePage(Page):
     lead_text = models.CharField(
@@ -45,7 +70,7 @@ class HomePage(Page):
                     template="streams/testimonial_block.html",
                 ),
             ),
-            ("pricing_table", blocks.PricingTableBlock()),
+            ("pricing_table", blocks.PricingTableBlock(table_options=new_table_options)),
         ],
         null=True,
         blank=True,
