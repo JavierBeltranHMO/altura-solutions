ea5fa5a60b0ad0b6ca80e757717a8bb8335bae42
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-15 17:31:11 -0700

    added richtext and blocks to misc page

--- misc/models.py
+++ misc/models.py
@@ -1,8 +1,50 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, PageChooserPanel
+from wagtail.fields import StreamField
+from wagtail.snippets.blocks import SnippetChooserBlock
+import wagtail.blocks as wagtail_blocks
+
+import streams.blocks as blocks
+from home.models import new_table_options
+
 
 class MiscPage(Page):
+
+    body = StreamField(
+        [
+            ("title", blocks.TitleBlock()),
+            ("cards", blocks.CardsBlock()),
+            ("image_and_text", blocks.ImageAndTextBlock()),
+            ("cta", blocks.CallToActionBlock()),
+            (
+                "testimonial",
+                SnippetChooserBlock(
+                    target_model="testimonials.Testimonial",
+                    template="streams/testimonial_block.html",
+                ),
+            ),
+            (
+                "pricing_table",
+                blocks.PricingTableBlock(table_options=new_table_options),
+            ),
+            (
+                "richtext",
+                wagtail_blocks.RichTextBlock(
+                    template="streams/simple_richtext_block.html",
+                    features=["bold", "italic", "ol", "ul", "link", "h3"],
+                ),
+            ),
+        ],
+        null=True,
+        blank=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
     class Meta:
-        verbose_name="Miscellaneous page"
-        verbose_name_plural="Miscellaneous pages"
+        verbose_name = "Miscellaneous page"
+        verbose_name_plural = "Miscellaneous pages"
