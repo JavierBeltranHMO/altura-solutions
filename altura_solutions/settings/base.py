3bf822296ede24a594c0dd10bc8be64fb84a42d7
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-09-02 16:59:47 -0700

    fixing static issues

--- altura_solutions/settings/base.py
+++ altura_solutions/settings/base.py
@@ -158,10 +158,10 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/src"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
